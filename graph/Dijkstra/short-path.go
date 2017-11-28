f600d9783a00310df32bd616bb95011106401623
heteddy <heteddy@126.com>
2017-11-28 18:55:35 +0800

    complete Dijkstra

--- graph/Dijkstra/short-path.go
+++ graph/Dijkstra/short-path.go
@@ -4,16 +4,23 @@ import (
 	"container/list"
 	"fmt"
 	"log"
+	"strconv"
 )
 
 //import "log"
 
 type WeightGraph map[GraphNode][]WeightPath
 type GraphNode string
+
 type WeightPath struct {
 	From   GraphNode
 	To     GraphNode
 	Weight int
+}
+
+type StepPath struct {
+	NodeList  *list.List
+	SumWeight int
 }
 
 func New(path []WeightPath) WeightGraph {
@@ -48,8 +55,8 @@ func (graph WeightGraph) GetAllNode() *list.List {
 	return nodeList
 }
 
-func (graph WeightGraph) ShortPath(startNode, endNode GraphNode) {
-	allNodes := graph.GetAllNode()
+/*
+allNodes := graph.GetAllNode()
 	if allNodes.Len() <= 1 {
 		return
 	}
@@ -76,5 +83,108 @@ func (graph WeightGraph) ShortPath(startNode, endNode GraphNode) {
 		noEnd += fmt.Sprintf(" %s", item.Value.(GraphNode))
 	}
 	log.Println(noEnd+"\n", "--------------------------")
+*/
+func (graph WeightGraph) ShortPath(startNode, endNode GraphNode) (ret bool) {
+	stepPathList := list.New()
+	//currentWeightPath, existed := graph[startNode]
+	//if !existed {
+	//	return
+	//}
+	//
+	//for _, p := range currentWeightPath {
+	//	nodeList := list.New()
+	//	log.Println(p)
+	//	nodeList.PushBack(p.From)
+	//	nodeList.PushBack(p.To)
+	//	s := StepPath{NodeList: nodeList, SumWeight: p.Weight}
+	//	stepPathList.PushBack(s)
+	//}
+	if ok := graph.initStepPathList(stepPathList, startNode); ok {
+		return graph.calculateShortPath(stepPathList, endNode)
+	}
 	return
+}
+
+func (graph WeightGraph) initStepPathList(stepPathList *list.List, start GraphNode) (ret bool) {
+	currentWeightPath, existed := graph[start]
+	if !existed {
+		//这种情况下没有路
+		return
+	} else {
+		ret = true
+	}
+	for _, p := range currentWeightPath {
+		nodeList := list.New()
+		nodeList.PushBack(p.From)
+		nodeList.PushBack(p.To)
+		s := StepPath{NodeList: nodeList, SumWeight: p.Weight}
+		stepPathList.PushBack(s)
+	}
+	delete(graph, start)
+	return
+}
+
+func (graph WeightGraph) calculateShortPath(stepPathList *list.List, end GraphNode) bool {
+
+	//2.遍历当前的stepPathList，获取其中最短的一条路径
+	//2.1 如果当前的最短路径已经是终点，而且没有其他的路径待计算，则返回当前的就是最短的，结束
+	//3.沿着最短路径继续向前进，获取下一个路径，如果这是一个终点（非end，但是没有路径），将当前的的路径从stepPathList中删除
+	//3.1.如果有下一条路径则修改stepPathList并重新计算SumWeight，然后从graph中删除当前的路径
+	//log.Println("----starting new caculcation----")
+	//for t := stepPathList.Front(); t != nil; t = t.Next() {
+	//	v := t.Value
+	//	for t1 := v.(StepPath).NodeList.Front(); t1 != nil; t1 = t1.Next() {
+	//		log.Println("node", t1.Value)
+	//	}
+	//	log.Println("weight", v.(StepPath).SumWeight)
+	//}
+	minItem := stepPathList.Front()
+	min := minItem.Value.(StepPath).SumWeight
+
+	for item := stepPathList.Front(); item != nil; item = item.Next() {
+		if item.Value.(StepPath).SumWeight < min {
+			minItem = item
+			min = minItem.Value.(StepPath).SumWeight
+		}
+	}
+	//2.1如果已经是终点,并且没有其他的路径需要计算，则返回最短的一条路劲
+	//查找结束
+	if minItem.Value.(StepPath).NodeList.Back().Value.(GraphNode) == end && len(graph) == 0 {
+		item := minItem.Value.(StepPath).NodeList
+		var output string
+		for i := item.Front(); i != nil; i = i.Next() {
+			output += fmt.Sprintf("%s ", i.Value.(GraphNode))
+		}
+		output += ":" + strconv.Itoa(minItem.Value.(StepPath).SumWeight)
+		log.Println(output)
+		return true
+	}
+	//取出minItem的最后链表的最后一项，即当前的终点，然后加入
+	candidateStart := minItem.Value.(StepPath).NodeList.Back().Value.(GraphNode)
+	//3.1如果不存在下一跳路径，则删除当前的路径
+	if currentWeightPath, existed := graph[candidateStart]; !existed {
+		stepPathList.Remove(minItem)
+		//如果没有路可选，说明从起点到终点没有一条可达的路径
+		if stepPathList.Len() == 0 {
+			return false
+		} else {
+			return graph.calculateShortPath(stepPathList, end)
+		}
+
+	} else {
+		delete(graph, candidateStart)
+		stepPathList.Remove(minItem)
+		for _, p := range currentWeightPath {
+			newList := list.New()
+			for mi := minItem.Value.(StepPath).NodeList.Front(); mi != nil; mi = mi.Next() {
+				newList.PushBack(mi.Value.(GraphNode))
+			}
+			newList.PushBack(p.To)
+			s := StepPath{NodeList: newList, SumWeight: p.Weight + minItem.Value.(StepPath).SumWeight}
+
+			stepPathList.PushBack(s)
+		}
+		return graph.calculateShortPath(stepPathList, end)
+	}
+
 }
--- graph/main.go
+++ graph/main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"algorithm/graph/Dijkstra"
+	//"log"
 	"log"
 )
 
-func main(){
-	p := make([]Dijkstra.WeightPath,0)
-	p = append(p,Dijkstra.WeightPath{"yuepu","changpian",5})
-	p = append(p,Dijkstra.WeightPath{"yuepu","haibao",0})
-	p = append(p,Dijkstra.WeightPath{"haibao","jita",30})
-	p = append(p,Dijkstra.WeightPath{"haibao","jiazigu",35})
-	p = append(p,Dijkstra.WeightPath{"changpian","jita",15})
-	p = append(p,Dijkstra.WeightPath{"changpian","jiazigu",20})
-	p = append(p,Dijkstra.WeightPath{"jita","gangqin",20})
-	p = append(p,Dijkstra.WeightPath{"jiazigu","gangqin",10})
+func main() {
+	p := make([]Dijkstra.WeightPath, 0)
+	p = append(p, Dijkstra.WeightPath{"yuepu", "changpian", 5})
+	p = append(p, Dijkstra.WeightPath{"yuepu", "haibao", 0})
+	p = append(p, Dijkstra.WeightPath{"haibao", "jita", 30})
+	p = append(p, Dijkstra.WeightPath{"haibao", "jiazigu", 35})
+	p = append(p, Dijkstra.WeightPath{"changpian", "jita", 15})
+	p = append(p, Dijkstra.WeightPath{"changpian", "jiazigu", 20})
+	p = append(p, Dijkstra.WeightPath{"jita", "gangqin", 20})
+	p = append(p, Dijkstra.WeightPath{"jiazigu", "gangqin", 10})
 	graph := Dijkstra.New(p)
-	log.Println(graph)
-	log.Println(graph.GetAllNode())
-	graph.ShortPath(Dijkstra.GraphNode("yuepu"),Dijkstra.GraphNode("gangqin"))
-	
+
+	if ok := graph.ShortPath(Dijkstra.GraphNode("yuepu"), Dijkstra.GraphNode("gangqin")); ok {
+		log.Println("success")
+	} else {
+		log.Println("failure")
+	}
+
 }
