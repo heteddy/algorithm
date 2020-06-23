2daf8e63a6e68263775ede3066fb23aae6b5f885
heteddy <heteddy@126.com>
2020-06-23 10:13:05 +0800

    修改spinlock

--- src/spinlock/spinlock.go
+++ src/spinlock/spinlock.go
@@ -28,7 +28,7 @@ func (sl *SpinLock) Lock() {
 		return
 	}
 	// 如果没获取到锁，则通过CAS自旋
-	for !atomic.CompareAndSwapUint32((*uint32)(&sl._lock), 0, 1) {
+	for !atomic.CompareAndSwapUint32(&sl._lock, 0, 1) {
 		runtime.Gosched()
 	}
 }
@@ -39,7 +39,7 @@ func (sl *SpinLock) Unlock() {
 	if sl.count > 0 { // 如果大于0，表示当前线程多次获取了该锁，释放锁通过count减一来模拟
 		sl.count--
 	} else { // 如果count==0，可以将锁释放，这样就能保证获取锁的次数与释放锁的次数是一致的了。
-		atomic.StoreUint32((*uint32)(&sl._lock), 0)
+		atomic.StoreUint32(&sl._lock, 0)
 	}
 }
 
