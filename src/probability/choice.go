1367e78b3061805ae5ccc30160b4160ee81f4256
heteddy <heteddy@126.com>
2020-06-24 09:09:31 +0800

    增加choise的说明

--- src/probability/choice.go
+++ src/probability/choice.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// 从n个数中等概率的选择m个数
 func PickMRecursively(input []int, m int) []int {
 	if m > len(input) {
 		return nil
@@ -19,7 +20,7 @@ func PickMRecursively(input []int, m int) []int {
 	for i := m; i < len(input); i++ {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		//fmt.Printf("%d ", r.Int31())
-
+		// 等概率的替换掉结果切片
 		k := r.Int() % i
 		// todo： 注意这里是k<m, 不是每次都换
 		if k < m {
